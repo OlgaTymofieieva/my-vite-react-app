1e5b7dbde5507584bd1980ed0b0d04587994f71d
OlgaTymofieieva <olha.tymofieieva@dci-student.org>
2024-10-17 14:38:01 +0200

    add Lorem

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
    <h1>Hallo</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, quae expedita recusandae, unde perferendis ut pariatur officia modi velit rem autem repellat quidem eius aliquid amet odio eum, enim praesentium?</p>
     </>
   )
 }
